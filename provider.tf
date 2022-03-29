98903e17c9797b3c7cc40c0214cafd1ce94cbc25
yutaokoshi <ookoshiyuuta@gmail.com>
2022-03-29 12:04:55 +0900

    fix

--- provider.tf
+++ provider.tf
@@ -9,5 +9,4 @@ terraform {
   # Optional attributes and the defaults function are
   # both experimental, so we must opt in to the experiment.
   experiments = [module_variable_optional_attrs]
-  version = "v0.0.1"
 }
